c8400e657df72119a204444af5bac723f1b2683d
Dimastri Mayco Putra <dimasmayco@gmail.com>
2018-07-10 11:43:41 +0700

    Create Project

---
+++ MaycosoftTestingFramework/MaycosoftTestingFramework/Maycosoft.swift
@@ -0,0 +1,19 @@
+//
+//  Maycosoft.swift
+//  MaycosoftTestingFramework
+//
+//  Created by Dimastri Mayco Putra on 10/07/18.
+//  Copyright © 2018 Maycosoft. All rights reserved.
+//
+
+import Foundation
+
+public class Mayco {
+    public static func string() -> String {
+        return "Uhyeah Yoman"
+    }
+    
+    public static func integer() -> Int {
+        return Int(arc4random())
+    }
+}
---
+++ MaycosoftTestingFramework/MaycosoftTestingFramework/MaycosoftTestingFramework.h
@@ -0,0 +1,19 @@
+//
+//  MaycosoftTestingFramework.h
+//  MaycosoftTestingFramework
+//
+//  Created by Dimastri Mayco Putra on 10/07/18.
+//  Copyright © 2018 Maycosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaycosoftTestingFramework.
+FOUNDATION_EXPORT double MaycosoftTestingFrameworkVersionNumber;
+
+//! Project version string for MaycosoftTestingFramework.
+FOUNDATION_EXPORT const unsigned char MaycosoftTestingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaycosoftTestingFramework/PublicHeader.h>
+
+
